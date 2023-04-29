2d75f17b6c2df27292812197b40d6443ed4a459c
taufiqraw <waskitaufiq@gmail.com>
2023-04-29 11:28:43 +0700

    Add Data diri

--- app/src/main/java/com/example/bloodshare/Home.kt
+++ app/src/main/java/com/example/bloodshare/Home.kt
@@ -3,6 +3,12 @@ package com.example.bloodshare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Tanggal Pengerjaan : 28-04-2023
+ * NIM      : 10120789
+ * Nama     : Taufiq Ridho Adi Waskita
+ * Kelas    : If-9
+ * **/
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
